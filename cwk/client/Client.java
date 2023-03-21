1ceee33d0ef752190079f2a8effacb7488c3592e
Brooklyn Mcswiney <brooklynmcswiney@gmail.com>
2023-03-21 09:17:54 +0000

    initial experimentation

--- cwk/client/Client.java
+++ cwk/client/Client.java
@@ -1,6 +1,73 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Client 
 {
+
+	public void connect()
+	{
+		// Initialise Reader and writer
+		Socket s = null;
+		PrintWriter socketOutput = null;
+		BufferedReader socketInputer = null;
+
+		try {
+			// Set up connection to the server
+			s = new Socket("localhost", 2323);
+			
+			// Set up writing stream with the server
+			socketOutput = new PrintWriter(s.getOutputStream(), true);
+
+			// Set up reading stream with the server
+			socketInputer = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		}
+		catch( UnknownHostException e)
+		{
+			System.err.println("Cannot connect to that host");
+			System.exit(1);
+		}
+		catch( IOException e)
+		{
+			System.err.println(e);
+			System.exit(1);
+		}
+
+		// Reader for the keyboard
+		BufferedReader stdIn = new BufferedReader(
+                                   new InputStreamReader(System.in) );
+        String fromServer;
+        String fromUser;
+
+		try {
+			while((fromServer=socketInputer.readLine()) != null)
+			{
+				// Echo response from the server
+				System.out.println("Server response: " + fromServer);
+
+				// Client types in response
+				fromUser = stdIn.readLine();
+				if(fromUser != null) {
+					// Echo client string.
+					System.out.println( "Client: " + fromUser );
+
+					// Write to server.
+					socketOutput.println(fromUser);
+				}
+			}
+			socketOutput.close();
+            socketInputer.close();
+            stdIn.close();
+            s.close();
+		}
+		catch(IOException e) {
+            System.err.println("I/O exception during execution\n");
+            System.exit(1);
+        }
+	}
 	public static void main( String[] args )
 	{
+		Client client = new Client();
+		client.connect();
 	}
 }
--- cwk/server/Server.java
+++ cwk/server/Server.java
@@ -1,6 +1,63 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Server 
 {
+	private ServerSocket serverSocket = null;
+
+	public Server() {
+		try {
+            serverSocket = new ServerSocket(2323);
+        }
+        catch (IOException e) {
+            System.err.println("Could not listen on port: 2323.");
+            System.exit(1);
+        }
+	}
+
+	public void runServer() {
+
+		while ( true )
+		{
+			Socket clientSocket = null;
+
+			try 
+			{
+				clientSocket = serverSocket.accept();
+			}
+			catch (IOException e)
+			{
+				System.err.println(e);
+				System.exit(1);
+			}
+
+			try
+			{
+				InetAddress inet = clientSocket.getInetAddress();
+				Date date = new Date();
+				System.out.println("\nDate " + date.toString());
+				System.out.println("connection made from " + inet.getHostName());
+	  
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				writer.println("Connection terminated.");
+				writer.close();
+				System.out.println("Connection terminated.");
+				clientSocket.close();
+			}
+			catch (IOException e)
+			{
+				System.err.println(e);
+				System.exit(1);
+			}
+			
+		}
+	}
+
 	public static void main( String[] args )
 	{
+		Server server = new Server();
+
+		server.runServer();
 	}
 }
